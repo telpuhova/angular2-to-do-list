79119d4760236c4c7dfac553e495789aca73da5c
telpuhova <telpuhova@gmail.com>
2018-02-19 21:17:10 -0800

    start adding view

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template:`
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li>{{firstTask.description}}</li>
+    </ul>
   </div>
   `
 })
@@ -15,5 +19,12 @@ import { Component } from '@angular/core';
 //class definition of Component
   //how a component behaves
 export class AppComponent {
-
+  currentFocus: string = 'angular homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask = {
+    description: "Finish weekend Angular homework for Epicodus course"
+  }
 }
