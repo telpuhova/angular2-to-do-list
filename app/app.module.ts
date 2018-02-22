8da0f9df48a810ba923ed58039bf9e15daf2eedc
telpuhova <telpuhova@gmail.com>
2018-02-22 12:26:03 -0800

    add two-way data binding: ability to dynamically edit tasks

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule }  from '@angular/forms';
 
 //annotation
 @NgModule({//module decorator
-  imports: [ BrowserModule ],//an array of other modules and content this module requires
+  imports: [ BrowserModule,
+              FormsModule ],//an array of other modules and content this module requires
   //^differs from the import statements at the top of the file. The import statements at the top import functionality from the Angular core. The imports array under the decorator imports other pieces of our application we want included in this module.
   declarations: [ AppComponent ],//array of all components that will reside in this module
   bootstrap: [ AppComponent ]//array of components required immediately upon launching the application
